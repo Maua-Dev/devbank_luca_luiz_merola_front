33a7147f31bc030e878c03262e58af9c743b98c0
Gabriel Merola <gabrielmerola12@gmail.com>
2024-04-05 11:39:50 -0300

    Tailwind installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import { useState } from 'react'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <h1>🚀 Vite React Template 🚀</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
-
-export default App
