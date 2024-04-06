fea92adea13fc5f98e8af9a3d1b39a16bde0913f
LucaPinheiro <lucapgomes11@gmail.com>
2024-04-06 14:05:49 -0300

    starting accont screen and adjusting images and containers with tailwind

--- src/screens/Account/index.tsx
+++ src/screens/Account/index.tsx
@@ -1,13 +1,36 @@
 import { Header } from "../../components/header";
 
 export function Account() {
-    return (
-        <>
-        <Header/>
+  return (
+    <>
+      <Header />
+
+      <main className="flex justify-center p-8">
+        <section className="mt-8">
+          <div className="flex flex-col gap-2 mb-8">
+            <h2 className="font-semibold text-2xl">Conta</h2>
+            <span className="font-bold text-3xl">R$ 1.356,98</span>
+          </div>
+
+          <section className="flex gap-12 max-md:flex-col">
+            <div className="flex flex-col gap-12">
+              <button className="bg-black flex rounded-3xl">
+                <span className="text-white text-xl m-4 absolute">
+                  {"\\> depósito"}
+                </span>
+                <img src={ImgDeposito} alt="" />
+              </button>
+              <button className="bg-black flex rounded-3xl">
+                <span className="text-white text-xl m-4 absolute">
+                  {"\\> saque"}
+                </span>
+                <img src={ImgSaque} alt="" />
+              </button>
+            </div>
+
+        </section>
         
-        <main>
-            
-        </main>
-        </>
-    )
+      </main>
+    </>
+  );
 }
