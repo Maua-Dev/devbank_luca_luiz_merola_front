6f572fd1e652c961fbe09d511acd2436f5d8b8b0
luiz-felipe-b <lfbalaminute@hotmail.com>
2024-04-22 04:12:39 -0300

    modal de adicionar cédulas

--- src/components/returnButton/index.tsx
+++ src/components/returnButton/index.tsx
@@ -1,8 +1,8 @@
 
 export function ReturnButton() {
     return (
-        <button className="bg-black rounded-2xl text-center p-4 min-w-[148px]">
-                <div className="font-['Montserrat'] text-2xl text-zinc-100">
+        <button className="rounded-2xl text-center p-4 min-w-[148px]">
+                <div className="font-['Montserrat'] text-2xl text-zinc-950">
                     Voltar
                 </div>
         </button>
