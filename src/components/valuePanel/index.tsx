227c3ce6fefc69f373eeb4f1b70a49863e8e36bb
EnricoSantarelli <enricosantarelli@hotmail.com>
2024-04-28 19:32:16 -0300

    fix warnings

--- src/components/valuePanel/index.tsx
+++ src/components/valuePanel/index.tsx
@@ -1,30 +1,27 @@
-import WithdrawButton from "../withdrawButton";
 import ReturnButton from "../returnButton";
 import ValueButton from "../valueButton";
-import { ReactNode } from "react";
-
 interface ValuePanelProps {
     setValueQty: (value: number, qty: number) => void;
     functionButton: () => JSX.Element;
 }
 
-const ValuePanel: React.FC<ValuePanelProps> = ({setValueQty, functionButton}) => {
+const ValuePanel: React.FC<ValuePanelProps> = ({ setValueQty, functionButton }) => {
     return (
         <div className="flex flex-col gap-2 justify-center">
             <div className="flex gap-2">
-                <ValueButton setValueQty={setValueQty} value={2}/>
-                <ValueButton setValueQty={setValueQty} value={5}/>
-                <ValueButton setValueQty={setValueQty} value={10}/>
-                <ValueButton setValueQty={setValueQty} value={20}/>
+                <ValueButton setValueQty={setValueQty} value={2} />
+                <ValueButton setValueQty={setValueQty} value={5} />
+                <ValueButton setValueQty={setValueQty} value={10} />
+                <ValueButton setValueQty={setValueQty} value={20} />
             </div>
             <div className="flex gap-2">
-                <ValueButton setValueQty={setValueQty} value={50}/>
-                <ValueButton setValueQty={setValueQty} value={100}/>
-                <ValueButton setValueQty={setValueQty} value={200}/>
+                <ValueButton setValueQty={setValueQty} value={50} />
+                <ValueButton setValueQty={setValueQty} value={100} />
+                <ValueButton setValueQty={setValueQty} value={200} />
                 {functionButton()}
             </div>
             <div className="flex">
-                <ReturnButton route="/conta"/>
+                <ReturnButton route="/conta" />
             </div>
         </div>
     )
